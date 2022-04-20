55f0f3c6e0aa1aa67d1a81289e4c4e9fc3dde524
Aarenoksitolikus <super.naxa2001@yandex.ru>
2022-04-20 14:41:48 +0300

    update tests

--- src/test/java/ru/dev/numbers/generator/controllers/NumbersControllerTest.java
+++ src/test/java/ru/dev/numbers/generator/controllers/NumbersControllerTest.java
@@ -6,14 +6,19 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+import ru.dev.numbers.generator.exceptions.NumberException;
 import ru.dev.numbers.generator.models.CarNumber;
 import ru.dev.numbers.generator.services.templates.NumbersService;
 
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
 
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -68,6 +73,46 @@ class NumbersControllerTest {
                     .andExpect(content().contentType("text/plain;charset=UTF-8"))
                     .andExpect(content().string("А001АА 116 RUS"));
         }
+
+        @Test
+        void on_problems_throws_exception() throws Exception {
+
+            doThrow(new NumberException("Available car numbers are over"))
+                    .when(numbersService)
+                            .getNextNumber();
+
+            mockMvc.perform(get("/next"))
+                    .andDo(print())
+                    .andExpect(status().isBadRequest())
+                    .andExpect(jsonPath("$.code", is(400)))
+                    .andExpect(jsonPath("$.message", is("Available car numbers are over")));
+
+            doThrow(new NumberException("Cannot increment car number"))
+                    .when(numbersService)
+                    .getNextNumber();
+
+            mockMvc.perform(get("/next"))
+                    .andDo(print())
+                    .andExpect(status().isBadRequest())
+                    .andExpect(jsonPath("$.code", is(400)))
+                    .andExpect(jsonPath("$.message", is("Cannot increment car number")));
+        }
+
+        @Test
+        void on_problems_with_registration_number_throws_exception() throws Exception {
+            when(numbersService.getNextNumber()).thenReturn(CarNumber.builder()
+                    .id(222L)
+                    .regNumber(1001)
+                    .regionCode(116)
+                    .series("ВХС")
+                    .build());
+
+            mockMvc.perform(get("/next"))
+                    .andDo(print())
+                    .andExpect(status().isBadRequest())
+                    .andExpect(jsonPath("$.code", is(400)))
+                    .andExpect(jsonPath("$.message", is("Incorrect registration number")));
+        }
     }
 
     @Nested
@@ -93,6 +138,36 @@ class NumbersControllerTest {
                     .andExpect(status().isOk())
                     .andExpect(content().contentType("text/plain;charset=UTF-8"))
                     .andExpect(content().string("Х492ОК 116 RUS"));
+        }
+
+        @Test
+        void on_problems_throws_exception() throws Exception {
+
+            doThrow(new NumberException("Available car numbers are over"))
+                    .when(numbersService)
+                    .getRandomNumber();
+
+            mockMvc.perform(get("/random"))
+                    .andDo(print())
+                    .andExpect(status().isBadRequest())
+                    .andExpect(jsonPath("$.code", is(400)))
+                    .andExpect(jsonPath("$.message", is("Available car numbers are over")));
+        }
+
+        @Test
+        void on_problems_with_registration_number_throws_exception() throws Exception {
+            when(numbersService.getRandomNumber()).thenReturn(CarNumber.builder()
+                    .id(223L)
+                    .regNumber(1001)
+                    .regionCode(116)
+                    .series("КАР")
+                    .build());
+
+            mockMvc.perform(get("/random"))
+                    .andDo(print())
+                    .andExpect(status().isBadRequest())
+                    .andExpect(jsonPath("$.code", is(400)))
+                    .andExpect(jsonPath("$.message", is("Incorrect registration number")));
         }
     }
 }
--- src/test/java/ru/dev/numbers/generator/services/realisations/NumbersServiceImplTest.java
+++ src/test/java/ru/dev/numbers/generator/services/realisations/NumbersServiceImplTest.java
@@ -4,13 +4,14 @@ import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import ru.dev.numbers.generator.exceptions.NumberException;
 import ru.dev.numbers.generator.models.CarNumber;
 import ru.dev.numbers.generator.repositories.NumbersRepository;
 import ru.dev.numbers.generator.services.templates.NumbersService;
 
 import java.util.Optional;
 
-import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.when;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -29,13 +30,6 @@ class NumbersServiceImplTest {
     @MockBean
     private NumbersRepository numbersRepository;
 
-    private CarNumber currentNumber = CarNumber.builder()
-            .id(1L)
-            .regionCode(116)
-            .regNumber(0)
-            .series("ААА")
-            .build();
-
     private final CarNumber FIRST_NUMBER = CarNumber.builder()
             .id(2L)
             .regionCode(116)
@@ -49,16 +43,26 @@ class NumbersServiceImplTest {
             .series("ААА")
             .build();
 
-    CarNumber incrementCarNumber() {
-        currentNumber.setRegNumber(currentNumber.getRegNumber() + 1);
-        currentNumber.setId(currentNumber.getId() + 1);
-        return currentNumber;
-    }
+    private final CarNumber THIRD_NUMBER = CarNumber.builder()
+            .id(4L)
+            .regionCode(116)
+            .regNumber(3)
+            .series("ААА")
+            .build();
 
-    @BeforeEach
-    void setUp() {
-        when(numbersRepository.save(any())).thenReturn(incrementCarNumber());
-    }
+    private final CarNumber LIMIT_NUMBER = CarNumber.builder()
+            .id(999L)
+            .regionCode(116)
+            .regNumber(999)
+            .series("ААА")
+            .build();
+
+    private final CarNumber NEW_NUMBER = CarNumber.builder()
+            .id(1000L)
+            .regionCode(116)
+            .regNumber(0)
+            .series("ААВ")
+            .build();
 
     @Nested
     @DisplayName("getRandom() is working")
@@ -75,25 +79,67 @@ class NumbersServiceImplTest {
 
         @Test
         void returns_correct_number_if_no_one_number_exist() {
-            when(numbersRepository.findByRegNumberAndSeriesAndRegionCode(0, "ААА", 116))
+            when(numbersRepository.findByRegNumberAndSeriesAndRegionCode(1, "ААА", 116))
                     .thenReturn(Optional.empty());
-            var a = numbersService.getNextNumber();
-            assertThat(a.getId(), is(equalTo(FIRST_NUMBER.getId())));
-            assertThat(a.getRegNumber(), is(equalTo(FIRST_NUMBER.getRegNumber())));
-            assertThat(a.getSeries(), is(equalTo(FIRST_NUMBER.getSeries())));
-            assertThat(a.getRegionCode(), is(equalTo(FIRST_NUMBER.getRegionCode())));
+            when(numbersRepository.save(any())).thenReturn(FIRST_NUMBER);
+
+            var result = numbersService.getNextNumber();
+            assertThat(result.getId(), is(equalTo(FIRST_NUMBER.getId())));
+            assertThat(result.getRegNumber(), is(equalTo(FIRST_NUMBER.getRegNumber())));
+            assertThat(result.getSeries(), is(equalTo(FIRST_NUMBER.getSeries())));
+            assertThat(result.getRegionCode(), is(equalTo(FIRST_NUMBER.getRegionCode())));
+        }
+
+        @Test
+        void returns_correct_number_if_there_is_one_number() {
+            when(numbersRepository.findByRegNumberAndSeriesAndRegionCode(1, "ААА", 116))
+                    .thenReturn(Optional.of(FIRST_NUMBER));
+            when(numbersRepository.findByRegNumberAndSeriesAndRegionCode(2, "ААА", 116))
+                    .thenReturn(Optional.empty());
+            when(numbersRepository.save(any())).thenReturn(SECOND_NUMBER);
+
+            var result = numbersService.getNextNumber();
+            assertThat(result.getId(), is(equalTo(SECOND_NUMBER.getId())));
+            assertThat(result.getRegNumber(), is(equalTo(SECOND_NUMBER.getRegNumber())));
+            assertThat(result.getSeries(), is(equalTo(SECOND_NUMBER.getSeries())));
+            assertThat(result.getRegionCode(), is(equalTo(SECOND_NUMBER.getRegionCode())));
         }
 
         @Test
         void returns_correct_number_if_there_are_some_numbers() {
-            var b = numbersService.getNextNumber();
-            System.out.println(b);
-            var a = numbersService.getNextNumber();
-            System.out.println(a);
-            assertThat(a.getId(), is(equalTo(FIRST_NUMBER.getId())));
-            assertThat(a.getRegNumber(), is(equalTo(FIRST_NUMBER.getRegNumber())));
-            assertThat(a.getSeries(), is(equalTo(FIRST_NUMBER.getSeries())));
-            assertThat(a.getRegionCode(), is(equalTo(FIRST_NUMBER.getRegionCode())));
+            when(numbersRepository.findByRegNumberAndSeriesAndRegionCode(1, "ААА", 116))
+                    .thenReturn(Optional.of(FIRST_NUMBER));
+            when(numbersRepository.findByRegNumberAndSeriesAndRegionCode(2, "ААА", 116))
+                    .thenReturn(Optional.of(SECOND_NUMBER));
+            when(numbersRepository.save(any())).thenReturn(THIRD_NUMBER);
+
+            var result = numbersService.getNextNumber();
+            assertThat(result.getId(), is(equalTo(THIRD_NUMBER.getId())));
+            assertThat(result.getRegNumber(), is(equalTo(THIRD_NUMBER.getRegNumber())));
+            assertThat(result.getSeries(), is(equalTo(THIRD_NUMBER.getSeries())));
+            assertThat(result.getRegionCode(), is(equalTo(THIRD_NUMBER.getRegionCode())));
+        }
+
+        @Test
+        void returns_correct_number_if_there_is_no_available_numbers_with_this_series() {
+            when(numbersRepository.findByRegNumberAndSeriesAndRegionCode(anyInt(), eq("ААА"), eq(116)))
+                    .thenReturn(Optional.of(LIMIT_NUMBER));
+            when(numbersRepository.findByRegNumberAndSeriesAndRegionCode(0, "ААВ", 116))
+                    .thenReturn(Optional.empty());
+            when(numbersRepository.save(any())).thenReturn(NEW_NUMBER);
+
+            var result = numbersService.getNextNumber();
+            assertThat(result.getId(), is(equalTo(NEW_NUMBER.getId())));
+            assertThat(result.getRegNumber(), is(equalTo(NEW_NUMBER.getRegNumber())));
+            assertThat(result.getSeries(), is(equalTo(NEW_NUMBER.getSeries())));
+            assertThat(result.getRegionCode(), is(equalTo(NEW_NUMBER.getRegionCode())));
+        }
+
+        @Test
+        void on_problems_with_amount_of_entities_throws_exception() {
+            when(numbersRepository.countNumbers())
+                    .thenReturn(1728000L);
+            assertThrows(NumberException.class, () -> numbersService.getNextNumber(), "Available car numbers are over");
         }
     }
 }
